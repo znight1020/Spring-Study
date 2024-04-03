efd36edd3801cd35a3876b438422c2b449419257
현수 <znight1020@naver.com>
2024-04-03 10:20:36 +0900

    [스프링 입문] section2. 스프링 웹 개발 기초

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,13 +3,47 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("data", "hello");
+        model.addAttribute("name", "hello");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam(value = "name") String name, Model model){ // required 는 default: true
+        model.addAttribute("name", "hello");
+        return "hello-mvc";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam(value = "name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam(value = "name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        return hello;
+    }
+
+    static class Hello{
+        String name;
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
