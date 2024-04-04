d2d2616ac77be07bcb12d67ba80080b9ca093ae0
현수 <znight1020@naver.com>
2024-04-04 17:48:00 +0900

    [스프링 입문]: section5. 회원 관리 예제 - 웹 MVC 개발

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello(Model model){
         model.addAttribute("name", "hello");
         return "hello";
