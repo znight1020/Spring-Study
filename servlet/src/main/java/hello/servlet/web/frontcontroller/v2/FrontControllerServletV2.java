3822431dbed5e5d9a15169abd303e9fcef823a1d
현수 <znight1020@naver.com>
2024-04-21 00:51:10 +0900

    [스프링 MVC - 1]: Section4. Model 추가 - v3

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -15,19 +15,19 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
+@WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*")
 public class FrontControllerServletV2 extends HttpServlet {
-    private Map<String, ControllerV2> controllerV1Map = new HashMap<>();
+    private Map<String, ControllerV2> controllerV2Map = new HashMap<>();
     public FrontControllerServletV2(){
-        controllerV1Map.put("/front-controller/v1/members/new-form", new MemberFormControllerV2());
-        controllerV1Map.put("/front-controller/v1/members/save", new MemberSaveControllerV2());
-        controllerV1Map.put("/front-controller/v1/members", new MemberListControllerV2());
+        controllerV2Map.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerV2Map.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerV2Map.put("/front-controller/v2/members", new MemberListControllerV2());
     }
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String requestURI = request.getRequestURI();
 
-        ControllerV2 controller = controllerV1Map.get(requestURI);
+        ControllerV2 controller = controllerV2Map.get(requestURI);
         if(controller == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
