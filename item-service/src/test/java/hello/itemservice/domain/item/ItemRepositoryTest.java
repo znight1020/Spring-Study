b604fa7f25958f2edf9b8b844c2887eee77c3375
이현수 <znight1020@naver.com>
2024-05-01 16:20:21 +0900

    [스프링 MVC - 1]: Section7. 도메인 개발 테스트

--- item-service/src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
+++ item-service/src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ItemRepositoryTest {
@@ -28,21 +30,38 @@ class ItemRepositoryTest {
     }
 
     @Test
-    void updateItem() {
+    void findAll() {
         // given
+        Item item1 = new Item("item1", 10000, 10);
+        Item item2 = new Item("item2", 20000, 20);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
 
         // when
-
+        List<Item> result = itemRepository.findAll();
         // then
+        Assertions.assertThat(result.size()).isEqualTo(2);
+        Assertions.assertThat(result).contains(item1, item2);
     }
 
     @Test
-    void findAll() {
+    void updateItem() {
         // given
+        Item item1 = new Item("item1", 10000, 10);
 
+        Item savedItem = itemRepository.save(item1);
+        Long itemId = savedItem.getId();
+        
         // when
+        Item updateParam = new Item("item2", 20000, 30);
+        itemRepository.update(itemId, updateParam);
 
         // then
+        Item findItem = itemRepository.findById(itemId);
+        Assertions.assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+        Assertions.assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+        Assertions.assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
     }
 
 }
