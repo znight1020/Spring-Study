cd3716b3f015f2c349b688a3cdde9ca4caba5dd5
이현수 <znight1020@naver.com>
2024-06-15 11:38:25 +0900

    [스프링 DB - 1]: Section6. 스프링 예외 추상화 적용

--- 스프링_DB_1편/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ 스프링_DB_1편/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -2,6 +2,7 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV4_2;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -14,7 +15,6 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -34,7 +34,7 @@ class MemberServiceV4Test {
     public static final String MEMBER_EX = "ex";
 
     @Autowired
-    private MemberRepositoryV4_1 memberRepository;
+    private MemberRepositoryV4_2 memberRepository;
     @Autowired
     private MemberServiceV4 memberService;
 
@@ -47,8 +47,9 @@ class MemberServiceV4Test {
         }
 
         @Bean
-        MemberRepositoryV4_1 memberRepository() {
-            return new MemberRepositoryV4_1(dataSource);
+        MemberRepositoryV4_2 memberRepository() {
+            return new MemberRepositoryV4_2(dataSource);
+            // return new MemberRepositoryV4_1(dataSource);
         }
 
         @Bean
