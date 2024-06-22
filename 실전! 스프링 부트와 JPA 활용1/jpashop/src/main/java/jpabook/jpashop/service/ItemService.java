8168cb08341cbcfbebcbf3cf5d417d951a91b528
이현수 <znight1020@naver.com>
2024-06-22 20:10:48 +0900

    [스프링 부트와 JPA 활용]: 웹 계층 개발 - 회원, 상품 cru

--- 실전! 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ 실전! 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,14 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) { // param: 파라미터로 넘어온 준영속 상대의 엔티티
+        Item findItem = itemRepository.findOne(itemId); // 같은 엔티티를 조회, findOne 으로 찾아온 이 객체는 영속성을 가지고 있다.
+        findItem.setName(name); // 데이터를 수정한다.
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
