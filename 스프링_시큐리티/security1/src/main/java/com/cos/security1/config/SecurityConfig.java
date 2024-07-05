6fa860b15fa70992b9a93d32e65bd7e7383f3157
이현수 <znight1020@naver.com>
2024-07-05 17:54:30 +0900

    [스프링 시큐리티]: Security Config 작성

---
+++ 스프링_시큐리티/security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((authorizeRequest) ->
+                        authorizeRequest
+                                // 지엽적인 순서대로 작성해야 한다.
+                                .requestMatchers("/user/**").authenticated() // 인증된 사용자만 user 경로에 접근할 수 있다.
+                                .requestMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER") // manager 경로 접근 시 인증 뿐 아니라 ADMIN 혹은 MANAGER 역할을 가지고 있어야 한다.
+                                .requestMatchers("/admin/**").hasAnyRole("ADMIN") // admin 경로 접근 시 인증 뿐 아니라 ADMIN 역할을 가지고 있어야 한다.
+                                .anyRequest().permitAll() // 그 이외 접근은 모두 허용한다.
+                )
+                .formLogin(configurer ->
+                        configurer.loginPage("/login")
+                )
+                //.formLogin(Customizer.withDefaults()) // 기존 Security 로그인 페이지 띄우기
+                .build();
+    }
+}
--- 스프링_시큐리티/security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ 스프링_시큐리티/security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -9,6 +10,43 @@ public class IndexController {
     @GetMapping({"", "/"})
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/user")
+    @ResponseBody
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    @ResponseBody
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    @ResponseBody
+    public String manager() {
+        return "manager";
+    }
+
+    // 스프링 시큐리티가 해당 주소를 낚아챈다.
+    @GetMapping("/login")
+    @ResponseBody
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    @ResponseBody
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/loginProc")
+    @ResponseBody
+    public String loginProc() {
+        return "회원가입 완료됨!";
     }
 
 
