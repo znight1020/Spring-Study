34313d549cbc3687c8ddc39cd83389de4d18f139
이현수 <znight1020@naver.com>
2024-07-05 14:39:26 +0900

    [자바 ORM 표준 JPA 프로그래밍]: 변경 감지, 1차 캐시, 준영속성

--- 자바_ORM_표준JPA_프로그래밍/basic/src/main/java/hello/basic/domain/Member.java
+++ 자바_ORM_표준JPA_프로그래밍/basic/src/main/java/hello/basic/domain/Member.java
@@ -2,11 +2,12 @@ package hello.basic.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
     @Id
     private Long id;
